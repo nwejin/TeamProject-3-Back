62a6e68377be0dc66b54dc707a40781ff8d4cd40
I00jey <cyj5056@gmail.com>
2024-01-17 10:03:08 +0900

    mvc 파일 정리중

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -2,20 +2,20 @@ const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
 const UserSchema = new Schema({
-  user_id: {
-    require: true,
-    unique: true,
-    type: String,
-  },
-  user_password: {
-    require: true,
-    type: String,
-  },
-  word_bookmark: {
-    ref: "WordSchema",
-    type: [String],
-  },
+    user_id: {
+        require: true,
+        unique: true,
+        type: String,
+    },
+    user_password: {
+        require: true,
+        type: String,
+    },
+    word_bookmark: {
+        ref: "WordSchema",
+        type: [String],
+    },
 });
 
-// 기본적으로 첫번째인자 + s로 생성된다. -> 세번째 인자로 설정한 값이 컬렉션 아래에 스키마로 들어가게 된다.
+// 기본적으로 첫번째인자 + s(소문자)로 생성된다. -> 세번째 인자로 설정한 값이 DB 컬렉션 아래에 스키마명으로 들어가게 된다.
 module.exports = mongoose.model("User", UserSchema);
