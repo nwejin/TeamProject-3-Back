c110c1987b23e3d6df0fd661feb1e093b0be6e70
wonheeki <wonluvv229@gmail.com>
2024-01-19 11:34:58 +0900

    회원가입 구현

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
+const cors = require("cors"); // 추가
 const express = require("express");
 const app = express();
 const mongoConnect = require("./models/index");
-const PORT = 8000;
-
 mongoConnect();
+
+const PORT = process.env.PORT || 5000;
+app.use(cors()); // 추가
 
 const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
