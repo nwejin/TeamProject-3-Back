2c681e766b4031885725d5618b365fa8ab2495fd
I00jey <cyj5056@gmail.com>
2024-01-19 17:13:47 +0900

    front connect

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,6 @@ router.get("/", (req, res) => {
     res.json({ message: "Server is running" });
 });
 
-// 프론트엔드에 데이터를 전송하는 엔드포인트
-router.get("/api/data", (req, res) => {
-    // 여기서 필요한 데이터를 조회하거나 가공합니다.
-    const responseData = "Hello from the backend!";
-
-    res.json(responseData);
-});
-
 router.get("/createUser", Cuser.userInsert);
 router.get("/getnews", Cnews.getNewsList);
 router.get("/savenews", Cnews.saveNewsData);
