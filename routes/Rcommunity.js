3ed479104161bc665cfa12657e458d1373d393ae
nwejin <chlwls4m@gmail.com>
2024-01-22 11:03:32 +0900

    1/22

--- routes/Rcommunity.js
+++ routes/Rcommunity.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 router.use(cors());
 
 router.get('/community', Ccommunity.community);
+
+// 글 읽기
+// 글 추가
 router.post('/community/uploadPost', Ccommunity.communityInsert);
+// 글 삭제
 
 module.exports = router;
