006cfa084ffbf5d7d73f4f7d75745c3e4fd56fd8
nwejin <chlwls4m@gmail.com>
2024-01-23 03:13:45 +0900

    1/23-2

--- routes/Rcommunity.js
+++ routes/Rcommunity.js
@@ -4,9 +4,10 @@ const router = require('express').Router();
 router.get('/', Ccommunity.community);
 
 // 글 추가
-router.post('/', Ccommunity.communityWrite);
+router.post('/write', Ccommunity.communityWrite);
 
 // 글 읽기
+router.get('/read', Ccommunity.communityRead);
 // 글 삭제
 
 module.exports = router;
