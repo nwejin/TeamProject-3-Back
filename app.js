fa313f7ccc56abbb4c5756f6413a4f06d2e81f01
wonheeki <wonluvv229@gmail.com>
2024-01-22 09:08:42 +0900

    로그인, 회원가입 완료

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
-const cors = require("cors"); // 추가
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const mongoConnect = require("./models/index");
+const cookieParser = require('cookie-parser');
 mongoConnect();
-
 const PORT = process.env.PORT || 5000;
-app.use(cors()); // 추가
+app.use(cors({
+    origin: 'http://localhost:3000', // 클라이언트의 도메인
+    credentials: true,
+}));
+const bodyParser = require("body-parser");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const Cuser = require("../controllers/Cuser");
 const router = require("express").Router();
+const cors = require("cors");
 
 router.get("/", (req, res) => {
     res.json({ message: "Server is running" });
@@ -21,6 +22,18 @@ router.post("/login", Cuser.userLogin);
 
 // 회원가입 기능
 router.post("/register", Cuser.userRegister);
+
+// 아이디 중복체크
+router.post("/idDuplicate", Cuser.userIdDuplicate);
+
+// 아이디 찾기
+router.post("/findId", Cuser.userFindId);
+
+// 비밀번호 찾기
+router.post("/findPw", Cuser.userFindPw);
+
+// 비밀번호 변경하기
+router.post("/changePw", Cuser.userChangePw);
 
 
 module.exports = router;
