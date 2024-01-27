d5878525c39f93c4dc06a7a9f5ba127e1d5b6781
wonheeki <wonluvv229@gmail.com>
2024-01-27 09:52:13 +0900

    마이페이지

--- controllers/Cmypage.js
+++ controllers/Cmypage.js
@@ -3,7 +3,24 @@ const { tokenCheck } = require('../utils/tokenCheck');
 
 exports.UserModify = async (req, res) => {
     try {
-        console.log(await tokenCheck(req));
-        res.send(await tokenCheck(req));
-    } catch (error) {}
+        // console.log(req.body.id);
+        const id = await tokenCheck(req);
+        const user = await UserSchema.find({
+            user_id: id,
+        });
+        console.log(id);
+        res.send({ info: user });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.hi = async (req, res) => {
+    try {
+        const id = await tokenCheck(req);
+        console.log(id);
+        res.send({ hi: id });
+    } catch (error) {
+        console.log(error);
+    }
 };
--- routes/Rmypage.js
+++ routes/Rmypage.js
@@ -1,6 +1,7 @@
 const Cmypage = require('../controllers/Cmypage');
 const router = require('express').Router();
 
-router.get('/UserModify', Cmypage.UserModify);
+router.post('/UserModify', Cmypage.UserModify);
+router.get('/hi', Cmypage.hi);
 
 module.exports = router;
