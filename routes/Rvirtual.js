137a4bbeec922f35e7f0cdf170f6222780073dd8
dydgjs200 <dydgjs2016@naver.com>
2024-01-29 18:23:32 +0900

    profitAndLoss

--- routes/Rvirtual.js
+++ routes/Rvirtual.js
@@ -3,6 +3,7 @@ const Csell = require('../controllers/CSell');
 
 router.post('/profit', Csell.post_profit);
 router.post('/record', Csell.post_showRecord);
+router.post('/profitandloss', Csell.post_ProfitAndLoss);
 router.get('/vocabulary', Csell.get_vocabulary);
 
 module.exports = router;
