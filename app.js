22972b2614f2810c7908f08177f0a358b46a85ff
wonheeki <wonluvv229@gmail.com>
2024-02-05 20:59:00 +0900

    아이디 찾기 수정

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('server OK');
-});
+app.get('/', (req, res) => {});
 const indexRouter = require('./routes/Ruser');
 app.use('/', indexRouter);
 
